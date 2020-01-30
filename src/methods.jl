5aea7f0a19a75f8027fdcb04e03f214424946b43
avinemimus <>
2020-01-30 14:44:49 +0300

    introduced coefficients of weight distribution

--- src/methods.jl
+++ src/methods.jl
@@ -1,16 +1,54 @@
 include("models.jl")
 
-function velocityDeltas(rocket::Rocket)
-    deltas = []
+function initialMass(rocket::Rocket)
     mass = rocket.mPayload
     for stage in rocket.stages
         mass += stage.mFull
     end
+    return mass
+end
+
+function velocityDeltas(rocket::Rocket)
+    deltas = []
+    mass = initialMass(rocket)
 
     for stage in rocket.stages
-        append!(deltas, stage.c * log(mass / (mass - stage.mFuel)))
+        push!(deltas, stage.c * log(mass / (mass - stage.mFuel)))
         mass -= stage.mFull
     end
 
     return deltas
+end
+
+function rs(rocket::Rocket)
+    r = []
+    mass = initialMass(rocket)
+
+    for stage in rocket.stages
+        push!(r, mass / (mass - stage.mFuel))
+        mass -= stage.mFull
+    end
+    return r
+end
+
+function λs(rocket::Rocket)
+    λ = []
+    mass = initialMass(rocket)
+
+    for stage in rocket.stages
+        push!(λ, (mass - stage.mFull) / (mass))
+        mass -= stage.mFull
+    end
+    return λ
+end
+
+function εs(rocket::Rocket)
+    ε = []
+    mass = initialMass(rocket)
+
+    for stage in rocket.stages
+        push!(ε, (stage.mFull - stage.mFuel) / (mass))
+        mass -= stage.mFull
+    end
+    return ε
 end
--- tests.jl
+++ tests.jl
@@ -6,8 +6,8 @@ stages = [Stage(368100, 331300, 2.9),
           Stage(55900,  50300,  2.9)]
 rocket = Rocket(mass0, stages)
 
+# 1. Tsiolkovsky equation
 deltas = velocityDeltas(rocket)
-
 println("Number of stages: $(length(rocket.stages))\n")
 for (number, stage) in enumerate(rocket.stages)
     println("Stage $(number)")
@@ -17,3 +17,14 @@ end
 
 deltaVSum = reduce(+, velocityDeltas(rocket))
 println("deltaVSum = $(deltaVSum)\n")
+
+# 2. Weight distribution coefficients. Note : λ + ε = 1/r
+(r, λ, ε)  = (rs(rocket), λs(rocket), εs(rocket))
+
+println("Mass numbers: $(r)")
+println("Payload coefficients: $(λ)")
+println("Construction coefficients: $(ε)\n")
+
+for i in 1:length(rocket.stages)
+    println("$(λ[i] + ε[i]) = $(1 / r[i])")
+end
